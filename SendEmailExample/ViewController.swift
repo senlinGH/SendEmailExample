6687489e3d309e7aed25a11a2572a40df8ee1f5e
senlin32 <senlin32@hotmail.com>
2021-03-17 18:45:51 +0800

    ＊建立寄信功能按鈕＊

--- SendEmailExample/ViewController.swift
+++ SendEmailExample/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import MessageUI
+import SafariServices
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UINavigationControllerDelegate, MFMailComposeViewControllerDelegate {
+    @IBOutlet weak var sendEmailBtn: UIButton! {
+        didSet {
+            sendEmailBtn.layer.cornerRadius = 5
+            sendEmailBtn.clipsToBounds = true
+        }
+    }
+    @IBAction func sendEmailAction(_ sender: Any) {
+        if MFMailComposeViewController.canSendMail() {
+            let vc = MFMailComposeViewController()
+            vc.mailComposeDelegate = self
+            vc.setSubject("聯絡我們｜給我們建議") // 標題
+            vc.setToRecipients(["xxxABC@gmail.com"])    // 收件者
+            vc.setMessageBody("<h1>Hello !</h1>", isHTML: true) // 信件內容
+            present(vc, animated: true)
+        } else {
+            guard let url = URL(string: "https://www.google.com") else { return }
+            let vc = SFSafariViewController(url: url)
+            present(vc, animated: true)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        controller.dismiss(animated: true)
     }
 
 
