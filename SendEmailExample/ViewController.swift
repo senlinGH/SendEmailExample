d04aca237923243f5252403dc6fa3e9f5b4fafb1
senlin32 <senlin32@hotmail.com>
2021-03-17 15:50:53 +0800

    Initial Commit

---
+++ SendEmailExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendEmailExample
+//
+//  Created by Lin Yi Sen on 2021/3/17.
+//  Copyright © 2021 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
